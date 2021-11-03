f7b99a3d2e122f9987cc58aa7f8f1bd47791719f
Andrey Alkhovik <kylaran70@gmail.com>
2021-11-03 12:48:57 +0300

    Created new project ASP.Net Core MVC + .Net Framework 4.7

---
+++ Gisparser/Gisparser/Controllers/ParserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gisparser.Controllers
+{
+    public class ParserController : Controller
+    {
+        // GET: Parser
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
